c0b4ab71df1ef9d5e5ff7db6746310c311077afa
Ernest Micklei <ernest.micklei@gmail.com>
2024-11-26 21:14:56 +0100

    add builder and some go datastructures

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/emicklei/xml2go
 
 go 1.23.0
+
+require golang.org/x/text v0.20.0
--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
+	"strings"
+
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
 )
 
 func main() {
 	f, _ := os.Open("./data/doc1.xml")
 	dec := xml.NewDecoder(f)
+	b := newBuilder()
 	for {
 		tok, err := dec.Token()
 		if tok == nil {
@@ -24,9 +30,9 @@ func main() {
 		}
 		switch elm := tok.(type) {
 		case xml.StartElement:
-			slog.Info("start", "elm", elm)
+			b.begin(elm)
 		case xml.EndElement:
-			slog.Info("end", "elm", elm)
+			b.end(elm)
 		case xml.ProcInst:
 			slog.Info("inst", "elm", string(elm.Inst))
 		case xml.CharData:
@@ -34,5 +40,99 @@ func main() {
 		default:
 			slog.Info("other", "elm", elm)
 		}
+	}
+}
+
+type gostruct struct {
+	name   string
+	fields map[string]gofield
+}
+
+func newStruct(name string) gostruct {
+	return gostruct{
+		name:   name,
+		fields: make(map[string]gofield),
+	}
+}
+
+func (s gostruct) addField(f gofield) {
+	// for now override
+	s.fields[f.name] = f
+}
+
+func (s gostruct) String() string {
+	buf := new(strings.Builder)
+	fmt.Fprintf(buf, "type %s struct {\n", s.name)
+	fmt.Fprintf(buf, "\tXMLName xml.Name `xml:\"%s\"`\n", s.name)
+	for _, each := range s.fields {
+		fmt.Fprintf(buf, "\t%s %s `xml:\"%s,attr\"`\n", each.name, each.typ, each.xmltag)
+	}
+	fmt.Fprintf(buf, "}\n")
+	return buf.String()
+}
+
+type gofield struct {
+	name   string
+	typ    string
+	xmltag string
+}
+
+type stack[T any] struct {
+	elements []T
+}
+
+func (s *stack[T]) push(elem T) {
+	s.elements = append(s.elements, elem)
+}
+
+func (s *stack[T]) pop() T {
+	elem := s.elements[len(s.elements)-1]
+	s.elements = s.elements[:len(s.elements)-1]
+	return elem
+}
+
+func (s *stack[T]) top() T {
+	return s.elements[len(s.elements)-1]
+}
+
+type builder struct {
+	structsMap  map[string]gostruct
+	structStack *stack[gostruct]
+	fieldStack  *stack[gofield]
+}
+
+func newBuilder() *builder {
+	return &builder{
+		structsMap:  make(map[string]gostruct),
+		structStack: new(stack[gostruct]),
+		fieldStack:  new(stack[gofield]),
+	}
+}
+
+var titler = cases.Title(language.English)
+
+func (b *builder) begin(elem xml.StartElement) {
+	if len(elem.Attr) > 0 {
+		s := newStruct(titler.String(elem.Name.Local))
+		for _, each := range elem.Attr {
+			s.addField(gofield{name: titler.String(each.Name.Local), typ: detectGoType(each.Value)})
+		}
+		b.structStack.push(s)
+	}
+	// no attr
+	f := gofield{name: titler.String(elem.Name.Local)}
+	b.structStack.top().addField(f)
+}
+func (b *builder) end(elem xml.EndElement) {
+
+}
+
+func detectGoType(valueString string) string {
+	// best guess
+	switch valueString {
+	case "true", "false":
+		return "boolean"
+	default:
+		return "string"
 	}
 }
