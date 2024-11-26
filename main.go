756580f9911057a517bf773f9d53226a207929f5
Ernest Micklei <ernest.micklei@gmail.com>
2024-11-26 22:43:38 +0100

    work on attrs

--- main.go
+++ main.go
@@ -34,12 +34,14 @@ func main() {
 		case xml.EndElement:
 			b.end(elm)
 		case xml.ProcInst:
-			slog.Info("inst", "elm", string(elm.Inst))
 		case xml.CharData:
-			slog.Info("data", "elm", string(elm))
+			b.data(string(elm))
 		default:
 			slog.Info("other", "elm", elm)
 		}
+	}
+	for _, each := range b.structsMap {
+		fmt.Println(each.String())
 	}
 }
 
@@ -65,7 +67,11 @@ func (s gostruct) String() string {
 	fmt.Fprintf(buf, "type %s struct {\n", s.name)
 	fmt.Fprintf(buf, "\tXMLName xml.Name `xml:\"%s\"`\n", s.name)
 	for _, each := range s.fields {
-		fmt.Fprintf(buf, "\t%s %s `xml:\"%s,attr\"`\n", each.name, each.typ, each.xmltag)
+		if each.isAttr {
+			fmt.Fprintf(buf, "\t%s %s `xml:\"%s,attr\"`\n", each.name, each.typ, each.xmltag)
+		} else {
+			fmt.Fprintf(buf, "\t%s %s `xml:\"%s\"`\n", each.name, each.typ, each.xmltag)
+		}
 	}
 	fmt.Fprintf(buf, "}\n")
 	return buf.String()
@@ -75,6 +81,7 @@ type gofield struct {
 	name   string
 	typ    string
 	xmltag string
+	isAttr bool
 }
 
 type stack[T any] struct {
@@ -95,10 +102,15 @@ func (s *stack[T]) top() T {
 	return s.elements[len(s.elements)-1]
 }
 
+func (s *stack[T]) empty() bool {
+	return len(s.elements) == 0
+}
+
 type builder struct {
 	structsMap  map[string]gostruct
 	structStack *stack[gostruct]
 	fieldStack  *stack[gofield]
+	lastDATA    string
 }
 
 func newBuilder() *builder {
@@ -111,20 +123,46 @@ func newBuilder() *builder {
 
 var titler = cases.Title(language.English)
 
+func (b *builder) data(text string) {
+	b.lastDATA = text
+}
+
 func (b *builder) begin(elem xml.StartElement) {
+	slog.Info("begin", "elem", elem.Name.Local)
 	if len(elem.Attr) > 0 {
 		s := newStruct(titler.String(elem.Name.Local))
 		for _, each := range elem.Attr {
-			s.addField(gofield{name: titler.String(each.Name.Local), typ: detectGoType(each.Value)})
+			s.addField(gofield{name: titler.String(each.Name.Local), isAttr: true, xmltag: each.Name.Local, typ: detectGoType(each.Value)})
 		}
 		b.structStack.push(s)
 	}
 	// no attr
-	f := gofield{name: titler.String(elem.Name.Local)}
-	b.structStack.top().addField(f)
+	f := gofield{name: titler.String(elem.Name.Local), xmltag: elem.Name.Local}
+	b.fieldStack.push(f)
 }
 func (b *builder) end(elem xml.EndElement) {
-
+	slog.Info("end", "elem", elem.Name.Local)
+	// end of struct or end of field
+	if !b.structStack.empty() {
+		s := b.structStack.top()
+		// closes top struct?
+		if s.name == titler.String(elem.Name.Local) {
+			b.structsMap[s.name] = s
+			b.structStack.pop()
+			return
+		}
+	}
+	// closes top field?
+	if !b.fieldStack.empty() {
+		f := b.fieldStack.pop()
+		if !b.structStack.empty() {
+			s := b.structStack.top()
+			f.typ = detectGoType(b.lastDATA)
+			s.addField(f)
+			return
+		}
+	}
+	slog.Warn("unmatched end", "elem", elem.Name.Local)
 }
 
 func detectGoType(valueString string) string {
